d3d50673b47addc1ddcd92a28d057e5137d87443
arundhaj <arundhaj@gmail.com>
2017-03-15 18:05:48 +0530

    added NseStockQuote model and updated UI.

--- finance_app/models.py
+++ finance_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+   
+class NseStockQuote(models.Model):
+    def __str__(self):
+        return "{0}-{1}".format(self.symbol, self.scriptdate)
+    
+    symbol = models.CharField(max_length=30)
+    scriptdate = models.DateTimeField()
+    series = models.CharField(max_length=30)
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    totaltradeqty = models.FloatField()
+    totaltrades = models.FloatField()
+    isin = models.CharField(max_length=30)
+    last = models.FloatField()
+    prevclose = models.FloatField()
+    totaltradevalue = models.FloatField()
--- finance_app/views.py
+++ finance_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from finance_app.models import NseStockQuote
+from django.http.response import JsonResponse
+
+import json
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    context = {}
+    icici_records = NseStockQuote.objects.values_list('scriptdate', 'close').filter(symbol='ICICIBANK').order_by('-scriptdate')
+    context = {
+        'icici_records': icici_records
+    }
 
     return render(request, 'finance_app/index.html', context)
+
+def get_quote(request):
+    # http://localhost:8000/analytics/api/compressor_cycle/D_a6-c9?startdate=1476489600&enddate=1479168000
+    symbol = request.GET.get('symbol')
+    
+    quotes = NseStockQuote.objects.values('scriptdate', 'close').filter(symbol=symbol).order_by('-scriptdate')
+    
+    quotes_list = [[quote['scriptdate'].timestamp(), quote['close']] for quote in quotes]
+
+    return JsonResponse(quotes_list, safe=False)
